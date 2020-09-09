bd962c43ba3193b1b898812db01e2a9989c006de
yahavi <yahavi@jfrog.com>
2020-09-09 13:17:29 +0300

    ..

--- src/main/java/org/jfrog/buildinfo/deployment/BuildInfoModelPropertyResolver.java
+++ src/main/java/org/jfrog/buildinfo/deployment/BuildInfoModelPropertyResolver.java
@@ -3,18 +3,19 @@ package org.jfrog.buildinfo.deployment;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.logging.Log;
-import org.jfrog.build.api.*;
+import org.jfrog.build.api.Agent;
+import org.jfrog.build.api.Build;
+import org.jfrog.build.api.BuildAgent;
+import org.jfrog.build.api.MatrixParameter;
 import org.jfrog.build.api.builder.BuildInfoMavenBuilder;
 import org.jfrog.build.extractor.clientConfiguration.ArtifactoryClientConfiguration;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
 
 import static org.jfrog.build.api.BuildInfoFields.*;
 import static org.jfrog.buildinfo.utils.Utils.getMavenVersion;
 
 /**
+ * Maven Build info builder that resolves the configuration from {@link ArtifactoryClientConfiguration}
+ *
  * @author yahavi
  */
 public class BuildInfoModelPropertyResolver extends BuildInfoMavenBuilder {
@@ -26,65 +27,66 @@ public class BuildInfoModelPropertyResolver extends BuildInfoMavenBuilder {
         this.logger = logger;
         resolveCoreProperties(session, clientConf);
         resolveProperties(clientConf);
+        resolveBuildAgent(clientConf);
+        addRunParameters(clientConf);
     }
 
-    public void resolveProperties(ArtifactoryClientConfiguration clientConf) {
-        artifactoryPrincipal(clientConf.publisher.getName());
-        artifactoryPluginVersion(clientConf.info.getArtifactoryPluginVersion());
-        principal(clientConf.info.getPrincipal());
-        parentName(clientConf.info.getParentBuildName());
-        parentNumber(clientConf.info.getParentBuildNumber());
-
-        String buildUrl = clientConf.info.getBuildUrl();
-        if (StringUtils.isNotBlank(buildUrl)) {
-            url(buildUrl);
-        }
-
-        Vcs vcs = new Vcs();
-        String vcsRevision = clientConf.info.getVcsRevision();
-        if (StringUtils.isNotBlank(vcsRevision)) {
-            vcs.setRevision(vcsRevision);
-            vcsRevision(vcsRevision);
-        }
-
-        BuildAgent buildAgent = new BuildAgent("Maven", getMavenVersion(getClass()));
-        buildAgent(buildAgent);
-
-        String agentName = StringUtils.firstNonBlank(clientConf.info.getAgentName(), buildAgent.getName());
-        String agentVersion = StringUtils.firstNonBlank(clientConf.info.getAgentVersion(), buildAgent.getVersion());
-        agent(new Agent(agentName, agentVersion));
-
-        artifactoryPrincipal(clientConf.publisher.getName());
-        artifactoryPluginVersion(clientConf.info.getArtifactoryPluginVersion());
-
-        for (Map.Entry<String, String> runParam : clientConf.info.getRunParameters().entrySet()) {
-            MatrixParameter matrixParameter = new MatrixParameter(runParam.getKey(), runParam.getValue());
-            addRunParameters(matrixParameter);
-        }
-    }
-
+    /**
+     * Resolve mandatory properties - build-name build-number and build-started.
+     *
+     * @param session    - The current maven session to extract the start time
+     * @param clientConf - Artifactory client configuration
+     */
     private void resolveCoreProperties(MavenSession session, ArtifactoryClientConfiguration clientConf) {
-        String buildNumber = clientConf.info.getBuildNumber();
-        if (StringUtils.isBlank(buildNumber)) {
-            buildNumber = Long.toString(System.currentTimeMillis());
-        }
+        String buildNumber = StringUtils.defaultIfBlank(clientConf.info.getBuildNumber(), Long.toString(System.currentTimeMillis()));
         number(buildNumber);
 
-        Date buildStartedDate = session.getRequest().getStartTime();
-        String buildStarted = clientConf.info.getBuildStarted();
-        if (StringUtils.isBlank(buildStarted)) {
-            buildStarted = new SimpleDateFormat(Build.STARTED_FORMAT).format(buildStartedDate);
-        }
+        long buildStartTime = session.getRequest().getStartTime().getTime();
+        String buildStarted = StringUtils.defaultIfBlank(clientConf.info.getBuildStarted(), Build.formatBuildStarted(buildStartTime));
         started(buildStarted);
 
-        String buildTimestamp = clientConf.info.getBuildTimestamp();
-        if (StringUtils.isBlank(buildTimestamp)) {
-            buildTimestamp = Long.toString(buildStartedDate.getTime());
-        }
         logResolvedProperty(BUILD_NAME, super.name);
         logResolvedProperty(BUILD_NUMBER, buildNumber);
         logResolvedProperty(BUILD_STARTED, buildStarted);
-        logResolvedProperty(BUILD_TIMESTAMP, buildTimestamp);
+    }
+
+    /**
+     * Resolve general optional properties.
+     *
+     * @param clientConf - Artifactory client configuration
+     */
+    private void resolveProperties(ArtifactoryClientConfiguration clientConf) {
+        artifactoryPluginVersion(clientConf.info.getArtifactoryPluginVersion());
+        artifactoryPrincipal(clientConf.publisher.getName());
+        parentNumber(clientConf.info.getParentBuildNumber());
+        parentName(clientConf.info.getParentBuildName());
+        vcsRevision(clientConf.info.getVcsRevision());
+        principal(clientConf.info.getPrincipal());
+        url(clientConf.info.getBuildUrl());
+    }
+
+    /**
+     * Resolve the build agent
+     *
+     * @param clientConf - Artifactory client configuration
+     */
+    private void resolveBuildAgent(ArtifactoryClientConfiguration clientConf) {
+        BuildAgent buildAgent = new BuildAgent("Maven", getMavenVersion(getClass()));
+        buildAgent(buildAgent);
+        String agentName = StringUtils.firstNonBlank(clientConf.info.getAgentName(), buildAgent.getName());
+        String agentVersion = StringUtils.firstNonBlank(clientConf.info.getAgentVersion(), buildAgent.getVersion());
+        agent(new Agent(agentName, agentVersion));
+    }
+
+    /**
+     * Add run parameters
+     *
+     * @param clientConf - Artifactory client configuration
+     */
+    private void addRunParameters(ArtifactoryClientConfiguration clientConf) {
+        clientConf.info.getRunParameters().entrySet().stream()
+                .map(param -> new MatrixParameter(param.getKey(), param.getValue()))
+                .forEach(this::addRunParameters);
     }
 
     private void logResolvedProperty(String key, String value) {
