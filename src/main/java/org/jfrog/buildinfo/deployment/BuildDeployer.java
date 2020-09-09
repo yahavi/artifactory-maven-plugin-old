691e38f965f307211761997e30e8beb568a64290
yahavi <yahavi@jfrog.com>
2020-09-09 11:42:58 +0300

    ..

--- src/main/java/org/jfrog/buildinfo/deployment/BuildDeployer.java
+++ src/main/java/org/jfrog/buildinfo/deployment/BuildDeployer.java
@@ -1,5 +1,6 @@
 package org.jfrog.buildinfo.deployment;
 
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.maven.plugin.logging.Log;
 import org.jfrog.build.api.Artifact;
@@ -12,93 +13,134 @@ import org.jfrog.build.extractor.clientConfiguration.client.ArtifactoryBuildInfo
 import org.jfrog.build.extractor.clientConfiguration.deploy.DeployDetails;
 import org.jfrog.build.extractor.retention.Utils;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 import static org.jfrog.buildinfo.utils.Utils.setChecksums;
 
 /**
+ * Deploy artifacts and publish build info.
+ *
  * @author yahavi
  */
 public class BuildDeployer {
 
+    private static final String LOG_PREFIX = "Artifactory Build info recorder: ";
     private final Log logger;
 
     public BuildDeployer(Log logger) {
         this.logger = logger;
     }
 
-    public void deploy(Build build, ArtifactoryClientConfiguration clientConf, Map<String, DeployDetails> deployableArtifactBuilders) {
-        Map<String, Set<DeployDetails>> deployableArtifactsByModule = prepareDeployableArtifacts(build, deployableArtifactBuilders);
-
-        logger.debug("Build Info Recorder: deploy artifacts: " + clientConf.publisher.isPublishArtifacts());
-        logger.debug("Build Info Recorder: publication fork count: " + clientConf.publisher.getPublishForkCount());
-        logger.debug("Build Info Recorder: publish build info: " + clientConf.publisher.isPublishBuildInfo());
-
-        BuildInfoClientBuilder buildInfoClientBuilder = new BuildInfoClientBuilder(logger);
-        if (isDeployArtifacts(clientConf, deployableArtifactsByModule)) {
-            try (ArtifactoryBuildInfoClient client = buildInfoClientBuilder.resolveProperties(clientConf)) {
-                new ModuleParallelDeployHelper().deployArtifacts(client, deployableArtifactsByModule, clientConf.publisher.getPublishForkCount());
-            }
-        }
-        if (clientConf.publisher.isPublishBuildInfo()) {
-            publishBuildInfo(clientConf, build, buildInfoClientBuilder);
+    /**
+     * Deploy artifacts and publish the build info.
+     *
+     * @param build               - The build to deploy
+     * @param clientConf          - Artifactory client configuration
+     * @param deployableArtifacts - The deployable artifacts
+     */
+    public void deploy(Build build, ArtifactoryClientConfiguration clientConf, Map<String, DeployDetails> deployableArtifacts) {
+        Map<String, Set<DeployDetails>> deployableArtifactsByModule = prepareDeployableArtifacts(build.getModules(), deployableArtifacts);
+        boolean isDeployArtifacts = isDeployArtifacts(clientConf, deployableArtifactsByModule);
+        boolean isPublishBuildInfo = isPublishBuildInfo(clientConf);
+        if (!isDeployArtifacts && !isPublishBuildInfo) {
+            // Nothing to deploy
             return;
         }
-        logger.info("Artifactory Build Info Recorder: publish build info set to false, build info will not be published...");
-    }
 
-    private void publishBuildInfo(ArtifactoryClientConfiguration clientConf, Build build, BuildInfoClientBuilder buildInfoClientBuilder) {
-        try (ArtifactoryBuildInfoClient client = buildInfoClientBuilder.resolveProperties(clientConf)) {
-            logger.info("Artifactory Build Info Recorder: Deploying build info ...");
-            Utils.sendBuildAndBuildRetention(client, build, clientConf);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        try (ArtifactoryBuildInfoClient client = new BuildInfoClientBuilder(logger).resolveProperties(clientConf)) {
+            if (isDeployArtifacts) {
+                logger.debug(LOG_PREFIX + "Publication fork count: " + clientConf.publisher.getPublishForkCount());
+                new ModuleParallelDeployHelper().deployArtifacts(client, deployableArtifactsByModule, clientConf.publisher.getPublishForkCount());
+            }
+            if (isPublishBuildInfo) {
+                logger.info(LOG_PREFIX + "Deploying build info ...");
+                Utils.sendBuildAndBuildRetention(client, build, clientConf);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(LOG_PREFIX + "Failed to deploy build info.", e);
         }
     }
 
+    /**
+     * Return true if should deploy artifacts.
+     *
+     * @param clientConf          - Artifactory client configuration
+     * @param deployableArtifacts - The deployable artifacts
+     * @return true if should deploy artifacts
+     */
     private boolean isDeployArtifacts(ArtifactoryClientConfiguration clientConf, Map<String, Set<DeployDetails>> deployableArtifacts) {
         if (!clientConf.publisher.isPublishArtifacts()) {
-            logger.info("Artifactory Build Info Recorder: deploy artifacts set to false, artifacts will not be deployed...");
+            logger.info(LOG_PREFIX + "deploy artifacts set to false, artifacts will not be deployed...");
             return false;
         }
         if (MapUtils.isEmpty(deployableArtifacts)) {
-            logger.info("Artifactory Build Info Recorder: no artifacts to deploy...");
+            logger.info(LOG_PREFIX + "no artifacts to deploy...");
             return false;
         }
         return true;
     }
 
-    private Map<String, Set<DeployDetails>> prepareDeployableArtifacts(Build build, Map<String, DeployDetails> deployableArtifactBuilders) {
+    /**
+     * Return true if should publish build info.
+     *
+     * @param clientConf - Artifactory client configuration
+     * @return true if should publish build info
+     */
+    private boolean isPublishBuildInfo(ArtifactoryClientConfiguration clientConf) {
+        if (!clientConf.publisher.isPublishBuildInfo()) {
+            logger.info(LOG_PREFIX + "publish build info set to false, build info will not be published...");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Prepare deployable artifacts by module map.
+     *
+     * @param modules             - The build modules
+     * @param deployableArtifacts - The deployable artifacts
+     * @return deployable artifacts by module map
+     */
+    private Map<String, Set<DeployDetails>> prepareDeployableArtifacts(List<Module> modules, Map<String, DeployDetails> deployableArtifacts) {
         Map<String, Set<DeployDetails>> deployableArtifactsByModule = new LinkedHashMap<>();
-        List<Module> modules = build.getModules();
         for (Module module : modules) {
+
+            // Create deployable artifacts set for current module
             Set<DeployDetails> moduleDeployableArtifacts = new LinkedHashSet<>();
-            List<Artifact> artifacts = module.getArtifacts();
-            if (artifacts != null) {
-                for (Artifact artifact : artifacts) {
-                    String artifactId = BuildInfoExtractorUtils.getArtifactId(module.getId(), artifact.getName());
-                    DeployDetails deployable = deployableArtifactBuilders.get(artifactId);
-                    if (deployable != null) {
-                        File file = deployable.getFile();
-                        setChecksums(file, artifact, logger);
-                        artifact.setRemotePath(deployable.getTargetRepository() + "/" + deployable.getArtifactPath());
-                        moduleDeployableArtifacts.add(new DeployDetails.Builder().
-                                artifactPath(deployable.getArtifactPath()).
-                                file(file).
-                                md5(artifact.getMd5()).
-                                sha1(artifact.getSha1()).
-                                addProperties(deployable.getProperties()).
-                                targetRepository(deployable.getTargetRepository()).
-                                packageType(DeployDetails.PackageType.MAVEN).
-                                build());
-                    }
+            for (Artifact moduleArtifact : ListUtils.emptyIfNull(module.getArtifacts())) {
+                String artifactId = BuildInfoExtractorUtils.getArtifactId(module.getId(), moduleArtifact.getName());
+                DeployDetails deployableArtifact = deployableArtifacts.get(artifactId);
+                if (deployableArtifact != null) {
+                    moduleDeployableArtifacts.add(createDeployDetails(deployableArtifact, moduleArtifact));
                 }
             }
+
+            // If the deployable artifacts set is not empty, add it to the results
             if (!moduleDeployableArtifacts.isEmpty()) {
                 deployableArtifactsByModule.put(module.getId(), moduleDeployableArtifacts);
             }
         }
         return deployableArtifactsByModule;
+    }
+
+    /**
+     * Create DeployDetails of an artifact to deploy.
+     *
+     * @param deployableArtifact - DeployDetails without sha1
+     * @param artifact           - Thw artifact to deploy
+     * @return DeployDetails of artifact to deploy
+     */
+    private DeployDetails createDeployDetails(DeployDetails deployableArtifact, Artifact artifact) {
+        setChecksums(deployableArtifact.getFile(), artifact, logger);
+        return new DeployDetails.Builder().
+                artifactPath(deployableArtifact.getArtifactPath()).
+                file(deployableArtifact.getFile()).
+                md5(artifact.getMd5()).
+                sha1(artifact.getSha1()).
+                addProperties(deployableArtifact.getProperties()).
+                targetRepository(deployableArtifact.getTargetRepository()).
+                packageType(DeployDetails.PackageType.MAVEN).
+                build();
     }
 }
